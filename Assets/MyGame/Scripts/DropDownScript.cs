c9e81871133a15b9ba33a4c6f4fa453f8c421270
bryanong <bryanong703@gmail.com>
2024-11-13 21:41:39 +0800

    Initial commit

---
+++ Assets/MyGame/Scripts/DropDownScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class DropDownScript : MonoBehaviour
+{
+    [SerializeField] private ActionBasedSnapTurnProvider snapTurn;
+    [SerializeField] private ActionBasedContinuousTurnProvider continuousTurn;
+
+    public void TurnProviderSelect(int index)
+    {
+        if (index == 0)
+        {
+            snapTurn.enabled = true;
+            continuousTurn.enabled = false;
+        }
+        if (index == 1)
+        {
+            snapTurn.enabled = false;
+            continuousTurn.enabled = true;
+        }
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
